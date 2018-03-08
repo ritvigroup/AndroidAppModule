34b53c8bfffa49439b2b38084f30df014aa2c141
sunil <Sunil112!((#>
2018-03-08 12:36:09 +0530

    08-03-2018

--- app/src/main/java/com/ritvi/kaajneeti/MainActivity.java
+++ app/src/main/java/com/ritvi/kaajneeti/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ritvi.kaajneeti;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCenter.start(getApplication(), "3e173e70-d1bd-4fc7-a9be-0c20d0d762b5",
+                Analytics.class, Crashes.class);
     }
 }
