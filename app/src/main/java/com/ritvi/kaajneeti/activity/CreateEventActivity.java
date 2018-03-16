ac712ab93a5428528ab1e82383915a9eaa054fa7
sunil <Sunil112!((#>
2018-03-16 17:46:41 +0530

    16-03-2018

---
+++ app/src/main/java/com/ritvi/kaajneeti/activity/CreateEventActivity.java
@@ -0,0 +1,348 @@
+package com.ritvi.kaajneeti.activity;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.ritvi.kaajneeti.R;
+import com.ritvi.kaajneeti.Util.FileUtils;
+import com.ritvi.kaajneeti.Util.TagUtils;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CreateEventActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener,TimePickerDialog.OnTimeSetListener{
+    private int PICK_IMAGE_REQUEST = 101;
+    private static final int CAMERA_REQUEST = 103;
+    @BindView(R.id.toolbar)
+    android.support.v7.widget.Toolbar toolbar;
+    @BindView(R.id.iv_camera)
+    ImageView iv_camera;
+    @BindView(R.id.iv_calendar)
+    ImageView iv_calendar;
+    @BindView(R.id.iv_event_image)
+    ImageView iv_event_image;
+    @BindView(R.id.et_date)
+    EditText et_date;
+    @BindView(R.id.et_time)
+    EditText et_time;
+    @BindView(R.id.iv_clock)
+    ImageView iv_clock;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+        ButterKnife.bind(this);
+
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        iv_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final PopupMenu menu = new PopupMenu(CreateEventActivity.this, view);
+
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuitem) {
+                        switch (menuitem.getItemId()) {
+                            case R.id.popup_camera:
+                                startCamera();
+                                break;
+                            case R.id.popup_gallery:
+                                selectProfilePic();
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                menu.inflate(R.menu.menu_profile_pic_option);
+                menu.show();
+            }
+        });
+
+        iv_calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar now = Calendar.getInstance();
+                DatePickerDialog dpd = DatePickerDialog.newInstance(
+                        CreateEventActivity.this,
+                        now.get(Calendar.YEAR),
+                        now.get(Calendar.MONTH),
+                        now.get(Calendar.DAY_OF_MONTH)
+                );
+                dpd.show(getFragmentManager(), "Datepickerdialog");
+            }
+        });
+
+        iv_clock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar now = Calendar.getInstance();
+                TimePickerDialog timePickerDialog=TimePickerDialog.newInstance(
+                        CreateEventActivity.this,
+                        now.get(Calendar.HOUR_OF_DAY),
+                        now.get(Calendar.MINUTE),
+                        true
+                );
+                timePickerDialog.show(getFragmentManager(),"TimePicker");
+            }
+        });
+
+    }
+
+    String pictureImagePath = "";
+
+    public void startCamera() {
+        String strMyImagePath = Environment.getExternalStorageDirectory() + File.separator + "temp.png";
+
+        pictureImagePath = strMyImagePath;
+        File file = new File(pictureImagePath);
+        Uri outputFileUri = Uri.fromFile(file);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            cameraIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            Uri contentUri = FileProvider.getUriForFile(getApplicationContext(), getPackageName() + ".fileProvider", file);
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri);
+
+        } else {
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+        }
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+
+    public void selectProfilePic() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_PICK);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    String image_path_string = "";
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (null == data)
+                    return;
+                Uri selectedImageUri = data.getData();
+                System.out.println(selectedImageUri.toString());
+                // MEDIA GALLERY
+                String selectedImagePath = getPath(
+                        CreateEventActivity.this, selectedImageUri);
+                Log.d("sun", "" + selectedImagePath);
+                if (selectedImagePath != null && selectedImagePath != "") {
+                    image_path_string = selectedImagePath;
+                    Log.d(TagUtils.getTag(), "selected path:-" + selectedImagePath);
+                    setProfilePic();
+                } else {
+                    Toast.makeText(CreateEventActivity.this, "Selected File is Corrupted", Toast.LENGTH_LONG).show();
+                }
+                System.out.println("Image Path =" + selectedImagePath);
+            }
+            return;
+        }
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            File imgFile = new File(pictureImagePath);
+            if (imgFile.exists()) {
+                Bitmap bmp = BitmapFactory.decodeFile(pictureImagePath);
+                bmp = Bitmap.createScaledBitmap(bmp, bmp.getWidth() / 4, bmp.getHeight() / 4, false);
+                String strMyImagePath = FileUtils.getChatDir();
+                File file_name = new File(strMyImagePath + File.separator + System.currentTimeMillis() + ".png");
+                FileOutputStream fos = null;
+
+                try {
+                    fos = new FileOutputStream(file_name);
+                    Log.d(TagUtils.getTag(), "taking photos");
+                    bmp.compress(Bitmap.CompressFormat.PNG, 100, fos);
+                    fos.flush();
+                    fos.close();
+                    image_path_string = file_name.toString();
+                    setProfilePic();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return;
+        }
+    }
+
+    public void setProfilePic() {
+        Glide.with(getApplicationContext())
+                .load(image_path_string)
+                .placeholder(R.mipmap.ic_launcher)
+                .error(R.mipmap.ic_launcher)
+                .dontAnimate()
+                .into(iv_event_image);
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public static String getPath(final Context context, final Uri uri) {
+
+        // check here to KITKAT or new version
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/"
+                            + split[1];
+                }
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"),
+                        Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{split[1]};
+
+                return getDataColumn(context, contentUri, selection,
+                        selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    public static String getDataColumn(Context context, Uri uri,
+                                       String selection, String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {column};
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection,
+                    selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri
+                .getAuthority());
+    }
+
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri
+                .getAuthority());
+    }
+
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri
+                .getAuthority());
+    }
+
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri
+                .getAuthority());
+    }
+
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        String date = dayOfMonth+"/"+(monthOfYear+1)+"/"+year;
+        et_date.setText(date);
+    }
+
+    @Override
+    public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+        String time = hourOfDay+":"+minute;
+        et_time.setText(time);
+    }
+}
